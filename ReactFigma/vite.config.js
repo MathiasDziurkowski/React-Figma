b340e0d1a3b5437e6f86b130b002615a45b771ed
Mathias Dziurkowski <128103391+MathiasDziurkowski@users.noreply.github.com>
2023-12-12 21:03:25 -0300

    adicionando PWA

--- ReactFigma/vite.config.js
+++ ReactFigma/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'Furniro Furniture Site',
+      short_name: 'Furniro',
+      description: 'Furniture site that have a lot of fabric f',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icons/32.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/32.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+]
 })
+
